6c071d52986de6609b6aa3453b24bfad191dc1ee
jaclyn.ashley.ramkissoon@live.mercer.edu <Jaclyn Ramkissoon>
2014-09-09 10:44:17 -0400

    exercise add

--- exercises/class_6/grade_calculator_infinite.rb
+++ exercises/class_6/grade_calculator_infinite.rb
@@ -1,22 +1,31 @@
-puts 'Give me your grades, one at a time. Type \'done\' when you are done.'
+puts 'Give me one of your grades.'
 
+grades = []
 
-# an array to keep all the grades
-grades = [] #grades = Array.new
+grade1 = gets.chomp
+grades.push(grade1.to_i)
 
-# keep getting grades until user enters 'done'
-while true
-    grade = gets.chomp
-    if (grade.downcase == 'done')
-        exit
-    else
-        grades.push(grade.to_i)
-    end
-end
+puts 'Give me a second grade.'
 
-total_points = 0
-grades.each do |grade|
-    total_points = total_points + grade
-end
+grade2 = gets.chomp
+grades.push(grade2.to_i)
 
-puts 'my average = ' + (total_points / grade.size).to_s
+puts 'Give me a third grade.'
+
+grade3 = gets.chomp
+grades.push(grade3.to_i)
+
+puts 'Give me a fourth grade.'
+
+grade4 = gets.chomp
+grades.push(grade4.to_i)
+
+puts 'Give me a fifth grade.'
+
+grade5 = gets.chomp
+grades.push(grade5.to_i)
+
+average1 = ((grades[0] + grades[1]) / 2).to_i
+average2 = ((grades[2] + grades[3] + grades[4]) / 3).to_i
+
+puts 'Your average is ' + (((0.5 * average1).to_i) + ((0.5 * average2).to_i)).to_s + '.'
---
+++ exercises/class_6/namemodified.rb
@@ -0,0 +1,13 @@
+puts 'Hello, what is your first name?'
+first = gets.chomp
+if first == 'Jaclyn'
+    puts 'What a lovely first name!'
+    else puts 'Meh. Your name is alright.'
+end
+puts 'What is your middle name?'
+middle = gets.chomp
+puts 'What is your last name?'
+last = gets.chomp
+puts 'Your full name is ' + first  + ' ' + middle  + ' ' + last
+puts 'There are ' + (first.length + middle.length + last.length).to_s + ' letters in your name.'
+puts 'Your name backwards is ' + last.reverse + ' ' + middle.reverse + ' ' + first.reverse
